2079d8e1dc98ff9fbe33cdc3d6a6a24d27d37aa1
rodgjame@isu.edu <0814dexter>
2015-04-18 22:40:32 -0700

    Initial check-in

---
+++ HitRave/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HitRave.MvcApplication" Language="C#" %>
---
+++ HitRave/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HitRave.Startup))]
+namespace HitRave
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
