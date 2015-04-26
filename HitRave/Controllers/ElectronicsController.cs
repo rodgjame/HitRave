faf962099a9ea1c2efa90c5b6e853ff579aea729
rodgjame <rodgjame@isu.edu>
2015-04-26 15:38:57 -0700

    Added Views and Contollers for pages

---
+++ HitRave/Controllers/ElectronicsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HitRave.Controllers
+{
+    public class ElectronicsController : Controller
+    {
+        // GET: /Electronics/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Electronics/Shop/
+        public ActionResult Shop()
+        {
+            return View();
+        }
+    }
+}
---
+++ HitRave/Controllers/MakeupController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HitRave.Controllers
+{
+    public class MakeupController : Controller
+    {
+        // GET: /Makeup/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Makeup/Shop/
+        public ActionResult Shop()
+        {
+            return View();
+        }
+    }
+}
